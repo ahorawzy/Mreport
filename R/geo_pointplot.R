1ef464c142d19b7ed5f45633a80aebe0500a3e0d
ahorawzy <lzyz09193@163.com>
2018-05-29 17:09:45 +0800

    First commit of geo_pointplot.R function.

---
+++ R/geo_pointplot.R
@@ -0,0 +1,35 @@
+geo_pointplot <- function(pointdf,region=FALSE,type=FALSE,
+                          map="OpenStreet"){
+
+  if(sum(is.na(pointdf)) != 0){
+    stop("There is NA value in the dataframe. Please clear it.")
+  }
+
+  m <- leaflet::leaflet(pointdf)
+  if(map == "OpenStreet"){
+    m <- leaflet::addTiles(m)
+  } else if(map == "amap"){
+    m <- leafletCN::amap(m)
+  } else if(map == "landform"){
+    m <- leaflet::addProviderTiles(m,"Esri.WorldImagery")
+  } else if(map == "watercolor"){
+    m <- leaflet::addProviderTiles(m,"Stamen.Watercolor")
+  }
+  if(is.character(region)){
+    reg <- leafletCN::leafletGeo(region)
+    m <- leaflet::addPolygons(m,data = reg,stroke = TRUE,smoothFactor = 1,
+                              fillOpacity = 0,weight = 3)
+  }
+  if(type == TRUE){
+    types <- unique(pointdf[,"type"])
+    ntype <- length(types)
+    cols <- grDevices::terrain.colors(ntype)
+    pal <- leaflet::colorFactor(cols,domain=types)
+    m <- leaflet::addCircleMarkers(m,lng=~lng,lat=~lat,label = ~label,
+                                   color = ~pal(type))
+    m <- leaflet::addLegend(m,"bottomright",pal = pal, values = types)
+  } else{
+    m <- leaflet::addCircleMarkers(m,lng=~lng,lat=~lat,label = ~label)
+  }
+  return(m)
+}
