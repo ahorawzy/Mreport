e9b31b3eaa21793f086d5712028dd4fe923a06a2
ahorawzy <lzyz09193@163.com>
2018-06-20 15:56:42 +0800

    Add function handle_splitatts to split attributes from station_atts.RData

--- R/handle_data.R
+++ R/handle_data.R
@@ -10,3 +10,15 @@ handle_selectindex <- function(database,samplename){
   newdf <- database[database[,1] %in% sample_base[[samplename]],]
   return(newdf)
 }
+
+#' @export
+handle_splitatts <- function(dfatts,attname,startpoint){
+  x <- lapply(station_atts, str_subset,attname)
+  x <- lapply(x, str_sub,startpoint,-2)
+  x <- as.matrix(x)
+  x <- as.data.frame(x)
+  x$V1 <- as.character(x$V1)
+  x$index <- rownames(x)
+  x <- x[x$V1!="character(0)",]
+  return(x)
+}
