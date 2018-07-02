08540452f7e52cc2cb362ae57f0b1f730f9d1316
ahorawzy <lzyz09193@163.com>
2018-07-02 17:43:32 +0800

    Add three similar functions for caculate group_mean and sort it.

---
+++ R/caculate_data.R
@@ -0,0 +1,35 @@
+#' @export
+caculate_carsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,attsname,summarise,
+                         Wmean = weighted.mean(cars,w=mileage))
+  x_wmean <- x_wmean[order(x_wmean$Wmean),]
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  rownames(x_wmean) <- 1:nrow(x_wmean)
+  return(x_wmean)
+}
+
+#' @export
+caculate_passcarsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,attsname,summarise,
+                         Wmean = weighted.mean(passenger_cars,w=mileage))
+  x_wmean <- x_wmean[order(x_wmean$Wmean),]
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  rownames(x_wmean) <- 1:nrow(x_wmean)
+  return(x_wmean)
+}
+
+#' @export
+caculate_frecarsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,attsname,summarise,
+                         Wmean = weighted.mean(freight_cars,w=mileage))
+  x_wmean <- x_wmean[order(x_wmean$Wmean),]
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  rownames(x_wmean) <- 1:nrow(x_wmean)
+  return(x_wmean)
+}
