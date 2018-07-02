0bab72b14a2e26a151c4be51a6a22f858b3c0e3f
ahorawzy <lzyz09193@163.com>
2018-07-02 17:42:27 +0800

    Delete save_raw for no use.

--- R/save_dataraw.R
+++
@@ -1,6 +0,0 @@
-#' @export
-save_raw <- function(filename){
-  pathname <- paste("D:\\R\\packages\\Mreport\\data-raw\\",
-                    substitute(filename),".Rdata",sep="")
-  save(filename,file=pathname)
-}
