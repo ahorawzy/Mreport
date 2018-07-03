f24abcfde03267eb5b8d0d4aaa4b840c080e0eb7
ahorawzy <lzyz09193@163.com>
2018-07-03 11:22:04 +0800

    Add 3 functions for caculate wmean by roadlevels.

---
+++ R/caculate_level_mean.R
@@ -0,0 +1,35 @@
+#' @export
+caculate_level_carsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,c(attsname,"level"),plyr::summarise,
+                         Wmean = weighted.mean(cars,w=mileage))
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  x_wmean <- reshape2::dcast(x_wmean,x_wmean[[attsname]]~x[["level"]])
+  names(x_wmean)[1] <- attsname
+  return(x_wmean)
+}
+
+#' @export
+caculate_level_passcarsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,c(attsname,"level"),plyr::summarise,
+                         Wmean = weighted.mean(passenger_cars,w=mileage))
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  x_wmean <- reshape2::dcast(x_wmean,x_wmean[[attsname]]~x[["level"]])
+  names(x_wmean)[1] <- attsname
+  return(x_wmean)
+}
+
+#' @export
+caculate_level_frecarsmean <- function(jd,attsname,na.rm = T){
+  x_wmean <- plyr::ddply(jd,c(attsname,"level"),plyr::summarise,
+                         Wmean = weighted.mean(freight_cars,w=mileage))
+  if (na.rm == T) {
+    x_wmean <- na.omit(x_wmean)
+  }
+  x_wmean <- reshape2::dcast(x_wmean,x_wmean[[attsname]]~x[["level"]])
+  names(x_wmean)[1] <- attsname
+  return(x_wmean)
+}
