1ef5e970ee1279874e2cb11a8a9f182f89882f91
ahorawzy <lzyz09193@163.com>
2018-07-04 11:03:33 +0800

    Add a function to caculate increase_ratio.

--- R/caculate_ratio.R
+++ R/caculate_ratio.R
@@ -1,10 +1,26 @@
-caculate_yearratio <- function(nowdf,thendf){
+#' @export
+caculate_developratio <- function(nowdf,thendf){
   nlen <- ncol(nowdf)
   attname <- names(nowdf)[1]
   x <- merge(nowdf,thendf,by=attname,all.x=TRUE)
   out <- x[1:nlen]
   for (i in 2:nlen) {
     out[i] <- x[i]/x[i+nlen-1]
+  }
+  return(out)
+}
+
+#' @export
+caculate_increaseratio <- function(nowdf,thendf){
+  nlen <- ncol(nowdf)
+  attname <- names(nowdf)[1]
+  x <- merge(nowdf,thendf,by=attname,all.x=TRUE)
+  out <- x[1:nlen]
+  for (i in 2:nlen) {
+    out[i] <- ((x[i]-x[i+nlen-1])/x[i+nlen-1])*100
+    out[i] <- round(out[i],3)
+    out[i] <- apply(out[i],1,as.character)
+    out[i] <- apply(out[i],1,paste,"%",sep="")
   }
   return(out)
 }
