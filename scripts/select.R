09df650d4260940bcace75222a1e5422e75cc0ce
ahorawzy <lzyz09193@163.com>
2018-07-09 16:15:47 +0800

    Add select for day-data

--- scripts/select.R
+++ scripts/select.R
@@ -1,8 +1,8 @@
-#' Select atts and direction
+#' Select atts and direction for month-data
 #'
 #' This function not only select attributes and rename them, but also select
 #' records whose direction is "DuanMian". So the nrow will shrink.
-
+#'
 select_atts <- function(df){
   df <- df[df[["行驶方向"]]=="断面",c("观测站编号","观测站名称","观测里程",
                                 "经度","纬度","省级名称","地市名称","区县名称",
@@ -10,5 +10,17 @@ select_atts <- function(df){
                                 "机动车速度","拥挤度")]
   names(df) <- c("index","label","mileage","lng","lat","province","city","county",
                  "direction","passenger_cars","freight_cars","cars","speed","crowd")
+  return(df)
+}
+
+#' Select atts and direction for day-data
+select_atts_forday <- function(df){
+  df <- df[df[["行驶方向"]]=="断面",c("观测站编号","观测站名称","观测里程",
+                                "经度","纬度","省级名称","地市名称","区县名称",
+                                "行驶方向","客车当量","货车当量","机动车当量",
+                                "机动车速度","拥挤度","日")]
+  names(df) <- c("index","label","mileage","lng","lat","province","city","county",
+                 "direction","passenger_cars","freight_cars","cars","speed","crowd",
+                 "day")
   return(df)
 }
