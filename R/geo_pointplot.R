b1e5fa9afff612bf538076d6eccf5c61ec8fcb3c
ahorawzy <lzyz09193@163.com>
2018-07-29 23:14:56 +0800

    geo_pointplot has been simplified. Closes #3.

--- R/geo_pointplot.R
+++ R/geo_pointplot.R
@@ -32,15 +32,15 @@
 #'
 #' @export
 
-geo_pointplot <- function(pointdf, region = FALSE, type = FALSE, map = "OpenStreet", popup = FALSE, na.rm = FALSE) {
+geo_pointplot <- function(pointdf, region = NULL, type = FALSE, map = "OpenStreet", popup = NULL, na.rm = FALSE) {
     if (na.rm) {
         pointdf <- na.omit(pointdf)
     }
-    
+
     if (sum(is.na(pointdf)) != 0) {
         stop("There is NA value in the dataframe. Please clear it.")
     }
-    
+
     m <- leaflet::leaflet(pointdf)
     if (map == "OpenStreet") {
         m <- leaflet::addTiles(m)
@@ -51,37 +51,25 @@ geo_pointplot <- function(pointdf, region = FALSE, type = FALSE, map = "OpenStre
     } else if (map == "watercolor") {
         m <- leaflet::addProviderTiles(m, "Stamen.Watercolor")
     }
-    if (is.character(region)) {
+    if (!is.null(region)) {
         reg <- leafletCN::leafletGeo(region)
         m <- leaflet::addPolygons(m, data = reg, stroke = TRUE, smoothFactor = 1, fillOpacity = 0, weight = 3)
     }
 
-    if (type == TRUE & popup == FALSE) {
+    if (isTRUE(type)){
         types <- unique(pointdf[, "type"])
         ntype <- length(types)
         if (ntype <= 5) {
-            cols <- as.character(wesanderson::wes_palette(n = ntype, name = "BottleRocket2"))
+          cols <- as.character(wesanderson::wes_palette(n = ntype, name = "BottleRocket2"))
         } else {
-            cols <- as.character(wesanderson::wes_palette(n = ntype, name = "Darjeeling1", type = "continuous"))
-        }
-        pal <- leaflet::colorFactor(cols, domain = types)
-        m <- leaflet::addCircleMarkers(m, lng = ~lng, lat = ~lat, label = ~label, color = ~pal(type))
-        m <- leaflet::addLegend(m, "bottomright", pal = pal, values = types)
-    } else if (type == FALSE & popup == FALSE) {
-        m <- leaflet::addCircleMarkers(m, lng = ~lng, lat = ~lat, label = ~label)
-    } else if (type == TRUE & popup == TRUE) {
-        types <- unique(pointdf[, "type"])
-        ntype <- length(types)
-        if (ntype <= 5) {
-            cols <- as.character(wesanderson::wes_palette(n = ntype, name = "BottleRocket2"))
-        } else {
-            cols <- as.character(wesanderson::wes_palette(n = ntype, name = "Darjeeling1", type = "continuous"))
+          cols <- as.character(wesanderson::wes_palette(n = ntype, name = "Darjeeling1", type = "continuous"))
         }
         pal <- leaflet::colorFactor(cols, domain = types)
         m <- leaflet::addCircleMarkers(m, lng = ~lng, lat = ~lat, label = ~label, color = ~pal(type), popup = ~popup)
         m <- leaflet::addLegend(m, "bottomright", pal = pal, values = types)
-    } else if (type == FALSE & popup == TRUE) {
+    } else {
         m <- leaflet::addCircleMarkers(m, lng = ~lng, lat = ~lat, label = ~label, popup = ~popup)
     }
+
     return(m)
 }
--- tests/testthat/test_geo_pointplot.R
+++ tests/testthat/test_geo_pointplot.R
@@ -2,9 +2,29 @@ library(Mreport)
 context("geo_pointplot")
 
 load_base()
+load_sample_base()
 
 test_that("This base function can works well",{
   geopp <- geo_pointplot(station_plot,na.rm = T)
+  expect_is(geopp,"leaflet")
+  expect_is(geopp,"htmlwidget")
+})
+
+test_that("region function can works well",{
+  geopp <- geo_pointplot(station_plot,na.rm = T, region = "China")
+  expect_is(geopp,"leaflet")
+  expect_is(geopp,"htmlwidget")
+})
+
+test_that("type function can works well",{
+  station_plot_s <- handle_mergeplot(samplebase = sample_base$roadhub,stationplot = station_plot)
+  geopp <- geo_pointplot(station_plot_s,na.rm = T, type = T)
+  expect_is(geopp,"leaflet")
+  expect_is(geopp,"htmlwidget")
+})
+
+test_that("popup function can works well",{
+  geopp <- geo_pointplot(station_plot,na.rm = T, popup = T)
   expect_is(geopp,"leaflet")
   expect_is(geopp,"htmlwidget")
 })
