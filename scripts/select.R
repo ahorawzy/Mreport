b1ad1128709758b8143196a68bd2bbca5d5f1293
ahorawzy <lzyz09193@163.com>
2018-09-20 15:20:39 +0800

    Add function select_atts_forym

--- scripts/select.R
+++ scripts/select.R
@@ -24,3 +24,14 @@ select_atts_forday <- function(df){
                  "day")
   return(df)
 }
+
+select_atts_forym <- function(df){
+  df <- df[df[["行驶方向"]]=="断面",c("观测站编号","观测站名称","观测里程",
+                                "经度","纬度","省级名称","地市名称","区县名称",
+                                "行驶方向","客车当量","货车当量","机动车当量",
+                                "机动车速度","拥挤度","观测年月")]
+  names(df) <- c("index","label","mileage","lng","lat","province","city","county",
+                 "direction","passenger_cars","freight_cars","cars","speed","crowd",
+                 "ym")
+  return(df)
+}
