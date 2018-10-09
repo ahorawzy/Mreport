b24f472ceede22d37127a75ea2413c525e68d522
ahorawzy <lzyz09193@163.com>
2018-10-09 13:25:54 +0800

    Add new function split_md

--- scripts/split.R
+++ scripts/split.R
@@ -7,3 +7,10 @@ split_ym <- function(df){
   df$观测年月 <- paste(df$年份,df$月份,sep = "-")
   return(df)
 }
+
+split_md <- function(df){
+  df$月 <- lubridate::month(df$观测日期)
+  df$日 <- lubridate::day(df$观测日期)
+  df$月日 <- paste(df$月,df$日,sep="-")
+  return(df)
+}
